830bad92ad15dc3127b933984a9fa591fdecf740
BrunaVersiani <rayaneversiani@gmail.com>
2023-10-20 20:45:39 -0300

    api rest

---
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,23 @@
+package med.voll.api.medico;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}") //exprecao regular de 4 a 7 digitos
+        String crm,
+        @NotNull
+        Especialidade especiaçidade,
+        @NotNull @Valid DadosEndereco endereco) {
+
+}
