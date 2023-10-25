9a6eede57fed3f0d52f026ae7af6782fd556f4d1
BrunaVersiani <rayaneversiani@gmail.com>
2023-10-24 21:24:59 -0300

    api rest

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("pacientes")
 public class PacienteController {
     
-
+    repository
 
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroPaciente dados){
