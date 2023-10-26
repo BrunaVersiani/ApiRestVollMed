c72bbd9314064296f7e31f7fc048df14391050f7
BrunaVersiani <rayaneversiani@gmail.com>
2023-10-25 23:01:03 -0300

    Configuracao do crud de pacientes.

--- api/src/main/resources/db/migration/V4__alter-table-medicos-add-column-ativo.sql
+++ api/src/main/resources/db/migration/V4__alter-table-medicos-add-column-ativo.sql
@@ -1,2 +1,2 @@
-alter table medicos add ativo tinyint;
+alter table medicos add column ativo tinyint;
 update medicos set ativo = 1;
--- api/src/main/resources/db/migration/V5__alter-table-pacientes-add-column-ativo.sql
+++ api/src/main/resources/db/migration/V5__alter-table-pacientes-add-column-ativo.sql
@@ -1,3 +1,3 @@
-alter table pacientes add collum ativo tinyint;
+alter table pacientes add column ativo tinyint;
 update pacientes set ativo = 1;
 alter table pacientes modify ativo tinyint not null;
